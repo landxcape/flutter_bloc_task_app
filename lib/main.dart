8540f3e3c86a94ff144ada9d7baa94c655c473ea
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2023-02-26 13:56:01 +0545

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,17 @@ import 'package:flutter_tasks_app/screens/counter/counter_screen.dart';
 import 'package:flutter_tasks_app/services/app_router.dart';
 import 'package:path_provider/path_provider.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'blocs/bloc_exports.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
