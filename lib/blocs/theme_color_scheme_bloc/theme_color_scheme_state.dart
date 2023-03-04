7846bcdf15e16eb7166278dc4a617eb236d6bba1
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2023-03-04 21:37:23 +0545

    Update theme_color_scheme_state.dart

--- lib/blocs/theme_color_scheme_bloc/theme_color_scheme_state.dart
+++ lib/blocs/theme_color_scheme_bloc/theme_color_scheme_state.dart
@@ -7,7 +7,7 @@ class ThemeColorSchemeState extends Equatable {
   });
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [colorScheme];
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
